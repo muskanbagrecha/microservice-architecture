34d7834c10edbfe81ee4b41fe51b41db53b7da3f
Muskan Bagrecha <muskanbagrecha04@gmail.com>
2023-08-02 18:20:44 +0530

    updated package name

--- service-communication/discovery-server/src/main/java/com/wf/discoveryserver/DiscoveryServerApplication.java
+++ service-communication/discovery-server/src/main/java/com/wf/discoveryserver/DiscoveryServerApplication.java
@@ -1,4 +1,4 @@
-package io.javabrains.discoveryserver;
+package com.wf.discoveryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- service-communication/discovery-server/src/test/java/com/wf/discoveryserver/DiscoveryServerApplicationTests.java
+++ service-communication/discovery-server/src/test/java/com/wf/discoveryserver/DiscoveryServerApplicationTests.java
@@ -1,4 +1,4 @@
-package io.javabrains.discoveryserver;
+package com.wf.discoveryserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
